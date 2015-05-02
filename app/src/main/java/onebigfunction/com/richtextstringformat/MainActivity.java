12bceab8a07cc258f39c14b6bab2239d2471af40
Gregory McQuillan <gmcquillan@gmail.com>
2015-05-01 21:21:26 -0700

    now using string formatting with rich text
    Instead of showing the price in the strings.xml itself using "buy now
    only <b>$0.99</b>" we now use a formatted string to programmatically
    place the price in.

--- app/src/main/java/onebigfunction/com/richtextstringformat/MainActivity.java
+++ app/src/main/java/onebigfunction/com/richtextstringformat/MainActivity.java
@@ -2,16 +2,64 @@ package onebigfunction.com.richtextstringformat;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int SKU_FIVE_CREDITS = 1;
+    private static final int SKU_TEN_CREDITS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView fiveCreditsDescriptionTextView
+                = (TextView)findViewById(R.id.five_credits_product_description);
+
+        TextView tenCreditsDescriptionTextView
+                = (TextView)findViewById(R.id.ten_credits_product_description);
+
+        String description;
+        if (fiveCreditsDescriptionTextView != null) {
+            description = getString(R.string.product_description_formatted,
+                    getString(R.string.five).toUpperCase(),
+                    getPrice(SKU_FIVE_CREDITS));
+            fiveCreditsDescriptionTextView.setText(Html.fromHtml(description));
+        }
+
+        if (tenCreditsDescriptionTextView != null) {
+            description = getString(R.string.product_description_formatted,
+                    getString(R.string.ten).toUpperCase(),
+                    getPrice(SKU_TEN_CREDITS));
+            tenCreditsDescriptionTextView.setText(Html.fromHtml(description));
+        }
+
+    }
+
+    /**
+     * Mock function to get the product price for a given sku. In a real
+     * application this would probably be in some other class.
+     *
+     * @param sku Product sku
+     * @return Returns price of the request product sku
+     */
+    private String getPrice(int sku) {
+        String price = "";
+        if (sku == SKU_FIVE_CREDITS) {
+            // again, realistically you would never return the prices like
+            // this. For simplicity's sake this is hard-coded as an example.
+            price = "$0.99";
+        }
+        else if (sku == SKU_TEN_CREDITS) {
+            price = "$4.99";
+        }
+
+        return price;
     }
 
 
